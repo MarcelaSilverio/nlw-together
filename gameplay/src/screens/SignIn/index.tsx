225f25bd2864cc8b76fbeb17d30e20dd95bc0fb1
Marcela Prata Silvério <marcelapsilverio@gmail.com>
2021-06-21 17:53:02 -0300

    First class

--- gameplay/src/screens/SignIn/index.tsx
+++ gameplay/src/screens/SignIn/index.tsx
@@ -1,26 +1,51 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StatusBar
+} from 'react-native';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IlustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
 
-    const [text, setText] = useState(''); //estados para armazenar valores
-
-
     return (
         <View style={styles.container}>
 
-            <Text>Hello World, NLW Together</Text>
-
-            <TextInput
-                style={styles.input}
-                onChangeText={setText} //(value) => console.log(value)
+            <StatusBar
+                barStyle='light-content'
+                backgroundColor='transparent'
+                translucent
             />
 
-            <Text>
-                Você digitou: {text}
-            </Text>
+            <Image
+                source={IlustrationImg}
+                style={styles.image}
+                resizeMode='stretch'
+            />
+
+            <View style={styles.content}>
+
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon
+                    title='Entrar com Discord'
+                    activeOpacity={0.7}
+                />
+
+            </View>
 
         </View>
     );
